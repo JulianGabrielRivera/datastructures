552409240c7cfe3dc39088bd1a25845d88319561
Julian Rivera <julianarevircoins@gmail.com>
2022-07-06 23:18:49 -0400

    another way for multiple pointers

--- index.js
+++ index.js
@@ -37,3 +37,31 @@ function sumZero(arr) {
   }
 }
 sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10]);
+
+function count(array) {
+  let first = array[0];
+  let second = array[1];
+
+  for (let i = 0; i < array.length; i++) {
+    if (first === second) {
+      second++;
+    } else {
+      first++;
+    }
+  }
+}
+
+// Multiple pointers
+
+let arr = [1, 2, 3, 4, 5, 6, 5, 5, 5, 6];
+
+function uniqueValues(arr) {
+  let i = 0;
+  for (let j = 1; j < arr.length; i++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
